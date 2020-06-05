99623c64e42d0ed9bc08f03b696d1d77151e94b2
ZainQudoos <65615089+ZainQudoos@users.noreply.github.com>
2020-06-05 15:05:10 +0500

    updated app.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <br>updated my me</br>
         </p>
         <a
           className="App-link"
